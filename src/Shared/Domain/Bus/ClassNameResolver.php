02f1400e93a64a1d6931094af847bd22c47e038d
jcamposforner <jcamposforner@gmail.com>
2020-01-06 17:22:34 +0100

    Synchronous query bus

--- src/Shared/Domain/Bus/ClassNameResolver.php
+++ src/Shared/Domain/Bus/ClassNameResolver.php
@@ -8,12 +8,15 @@ use ReflectionException;
 use ReflectionClass;
 use ReflectionMethod;
 use function Lambdish\Phunctional\map;
-use function Lambdish\Phunctional\reduce;
 use function Lambdish\Phunctional\reindex;
 
 final class ClassNameResolver
 {
-
+    /**
+     * @param $class
+     * @return string|null
+     * @throws ReflectionException
+     */
     public function extract($class): ?string
     {
         $reflector = new ReflectionClass($class);
@@ -26,28 +29,47 @@ final class ClassNameResolver
         return null;
     }
 
+    /**
+     * @param iterable $callables
+     * @return array
+     */
     public static function forCallables(iterable $callables): array
     {
         return map(self::unflatten(), reindex(self::classExtractor(new self()), $callables));
     }
 
-
+    /**
+     * @param ReflectionMethod $method
+     * @return string
+     */
     private function firstParameterClassFrom(ReflectionMethod $method): string
     {
         return $method->getParameters()[0]->getClass()->getName();
     }
 
+    /**
+     * @param ReflectionMethod $method
+     * @return bool
+     */
     private function hasOnlyOneParameter(ReflectionMethod $method): bool
     {
         return $method->getNumberOfParameters() === 1;
     }
 
+    /**
+     * @param ClassNameResolver $parameterExtractor
+     * @return callable
+     */
     private static function classExtractor(self $parameterExtractor): callable
     {
         return function (callable $handler) use ($parameterExtractor): string {
             return $parameterExtractor->extract($handler);
         };
     }
+
+    /**
+     * @return callable
+     */
     private static function unflatten(): callable
     {
         return function ($value) {
